9c44ecf51d0a32cc4452276be0ff3eb01282196b
fernando.cruz <fernandoluna.cruz-ext@xpollens.com>
2021-12-05 21:30:10 +0100

    add project

---
+++ IdentityServer/Configuration/Resources.cs
@@ -0,0 +1,83 @@
+﻿
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
+
+using System.Collections.Generic;
+
+namespace IdentityServer.Configuration
+{
+    public static class Resources
+    {
+
+
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                // backward compat
+                new ApiScope("ApiOne"),
+                new ApiScope("ApiTwo")
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            //new IdentityResources.Profile(),
+            new IdentityResource
+            {
+                Name = "rc.scope",
+                UserClaims =
+                {
+                    "rc.garndma"
+                }
+            }
+        };
+
+        public static IEnumerable<ApiResource> GetApiResources() =>
+        new List<ApiResource> {
+            new ApiResource("ApiOne", new string[] { "read", "write" }),
+        };
+
+        public static IEnumerable<Client> GetClients() =>
+        new List<Client> {
+
+            new Client {
+
+                ClientId = "client_id",
+                ClientSecrets = { new Secret("client_secret".ToSha256()) },
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = { "ApiOne" }
+            },
+
+            new Client {
+
+                ClientId = "client_id_mvc",
+                ClientSecrets = { new Secret("client_secret_mvc".ToSha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = true,
+
+                RedirectUris = { "https://localhost:4001/signin-oidc" },
+                PostLogoutRedirectUris = { "https://localhost:4001/Home/Index" },
+
+                AllowedScopes = {
+                    "ApiOne",
+                    "ApiTwo",
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    //IdentityServerConstants.StandardScopes.Profile,
+                    "rc.scope",
+                },
+
+                // puts all the claims in the id token
+                //AlwaysIncludeUserClaimsInIdToken = true,
+                AllowOfflineAccess = true,
+                RequireConsent = false,
+            },
+
+        };
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,98 @@
+using IdentityServer.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(config =>
+            {
+                config.UseSqlServer(connectionString);
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>(config =>
+            {
+                config.Password.RequiredLength = 4;
+                config.Password.RequireDigit = false;
+                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireUppercase = false;
+            })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(config =>
+            {
+                config.Cookie.Name = "IdentityServer.Cookie";
+                config.LoginPath = "/Auth/Login";
+                config.LogoutPath = "/Auth/Logout";
+            });
+
+            services.AddIdentityServer()
+                .AddAspNetIdentity<IdentityUser>()
+                //.AddConfigurationStore(options =>
+                //{
+                //    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                //        sql => sql.MigrationsAssembly(assembly));
+                //})
+                //.AddOperationalStore(options =>
+                //{
+                //    options.ConfigureDbContext = b => b.UseSqlServer(connectionString,
+                //        sql => sql.MigrationsAssembly(assembly));
+                //})
+                //.AddSigningCredential(certificate);
+                .AddInMemoryApiScopes(IdentityServer.Configuration.Resources.GetApiScopes())
+                .AddInMemoryApiResources(IdentityServer.Configuration.Resources.GetApiResources())
+                .AddInMemoryIdentityResources(IdentityServer.Configuration.Resources.GetIdentityResources())
+
+                .AddInMemoryClients(IdentityServer.Configuration.Resources.GetClients())
+                //.AddInMemoryClients(Configuration.GetSection("IdentityServer:Clients"))
+
+                .AddDeveloperSigningCredential();
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            if (env.IsDevelopment())
+            {
+                app.UseCookiePolicy(new CookiePolicyOptions()
+                {
+                    MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.Lax
+                });
+            }
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
